1dd86813bddb6afa89a6a20fc8fd630a9b3a7dee
ISAAC SOLOMON <G00424127@atu.ie>
2024-05-08 17:45:31 +0100

    Added some comments

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,12 +7,17 @@ import { NewsArticlesService } from '../api/news-articles.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+   // Default selected category
   selectedCategory = 'health';
+   // Array to store top headlines
   topHeadLines: any= [];
+   
   constructor(private articleService:NewsArticlesService) {
+    // Gets top headlines when the component is initialized
     articleService.getTopHeadLines().subscribe((results)=>{
-     // console.log('results:', results);
+     // Push the retrieved top headlines into the topHeadLines array
       this.topHeadLines.push(...results.articles);
+        // Log the retrieved articles to the console
       console.log(results.articles);
     })
     articleService.getArticlesByCategory(this.selectedCategory).subscribe((results)=>{
