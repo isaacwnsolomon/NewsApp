ea0bf2b6d62e069cabe6e564cdfdf9752716ab0c
ISAAC SOLOMON <G00424127@atu.ie>
2024-05-09 14:24:36 +0100

    Now able to navigate to details page from the favourites page

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { favouritesService } from '../favouriteService/favourite-articles.service';
+import { NavigationExtras, Router } from "@angular/router";
 
 @Component({
   selector: 'app-favourites',
@@ -10,7 +11,7 @@ export class FavouritesPage implements OnInit {
 
   favourites: any[] = [];
   
-  constructor(private favouritesService: favouritesService) {}
+  constructor(private favouritesService: favouritesService,private router: Router) {}
 
   ngOnInit() {
     this.favourites = this.favouritesService.getFavourites();
@@ -18,4 +19,21 @@ export class FavouritesPage implements OnInit {
   readMore(url: string) {
     window.open(url, '_blank');
   }
+  getDetails(selectedArticle: any){
+    //console.log(selectedArticle);
+    //creating naviagtion parameters
+    const params : NavigationExtras={
+      queryParams:{
+        author: selectedArticle.author ,
+        content: selectedArticle.content,
+        description: selectedArticle.description ,
+        publishedAt: selectedArticle.publishedAt,
+        source: selectedArticle.source.name ,
+        title: selectedArticle.title,
+        url: selectedArticle.url, 
+        urlToImage: selectedArticle.urlToImage, 
+  
+      }
 }
+this.router.navigate(['/details'], params);
+  }}
