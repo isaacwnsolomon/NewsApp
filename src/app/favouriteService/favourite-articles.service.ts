67d76973cec805d7862fe46ab4d4e577e2ae1a0f
ISAAC SOLOMON <G00424127@atu.ie>
2024-05-10 07:14:43 +0100

    Finally got favourites page to save to storage

--- src/app/favouriteService/favourite-articles.service.ts
+++ src/app/favouriteService/favourite-articles.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class favouritesService {
+  private favourites: any[] = [];
 
-  favourites: any[] = [];
+  constructor(private storage: Storage) {
+    this.init();
+  }
 
-  constructor() { }
+  async init() {
+    await this.storage.create();
+    this.favourites = (await this.storage.get('favourites')) || [];
+  }
 
   addToFavourites(article: any) {
     this.favourites.push(article);
+    this.saveFavourites();
   }
 
   getFavourites() {
     return this.favourites;
+  }
+
+  async saveFavourites() {
+    await this.storage.set('favourites', this.favourites);
   }
 }
--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { favouritesService } from '../favouriteService/favourite-articles.service';
-import { NavigationExtras, Router } from "@angular/router";
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-favourites',
@@ -8,22 +8,21 @@ import { NavigationExtras, Router } from "@angular/router";
   styleUrls: ['./favourites.page.scss'],
 })
 export class FavouritesPage implements OnInit {
-
   favourites: any[] = [];
-  
-  constructor(private favouritesService: favouritesService,private router: Router) {}
+
+  constructor(private favouritesService: favouritesService, private router: Router) {}
 
   ngOnInit() {
+    this.loadFavourites();
+  }
+
+  loadFavourites() {
     this.favourites = this.favouritesService.getFavourites();
   }
-  readMore(url: string) {
-    window.open(url, '_blank');
-  }
-  getDetails(selectedArticle: any){
-    //console.log(selectedArticle);
-    //creating naviagtion parameters
-    const params : NavigationExtras={
-      queryParams:{
+
+  getDetails(selectedArticle: any) {
+    const params: NavigationExtras = {
+      queryParams: {
         author: selectedArticle.author ,
         content: selectedArticle.content,
         description: selectedArticle.description ,
@@ -34,9 +33,13 @@ export class FavouritesPage implements OnInit {
         urlToImage: selectedArticle.urlToImage, 
   
       }
-}
+};
 this.router.navigate(['/details'], params);
   }
   goHome(){
     this.router.navigate(['/home']);
-  }}
+  }
+  readMore(url: string) {
+    window.open(url, '_blank');
+  }
+}
